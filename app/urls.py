a65c2d5cab89f820d79d8e3af1276f8cfa4b27a2
AsBergs <anasofiabergsoares06@gmail.com>
2024-07-30 08:09:13 -0300

    nsei cara

---
+++ app/urls.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
+
+router = DefaultRouter()
+
+router.register(r"autores", AutorViewSet, basename="autores")
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"editoras", EditoraViewSet, basename="editoras")
+router.register(r"usuarios", UserViewSet, basename="usuarios")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # OpenAPI 3
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    # Simple JWT
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # API
+    path("api/", include(router.urls)),
+]
